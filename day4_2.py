821d5f3b6ea2e7f068b78c8b4eff23c8dc73d42e
MinhWuyn <nguyenduyminh0205@gmail.com>
2024-04-19 10:15:17 +0700

    import library to use function Counter

---
+++ day4_2.py
@@ -0,0 +1,10 @@
+import collections
+
+def find_it(seq):
+    if len(seq) == 0:
+        return None
+    b = collections.Counter(seq)
+    for c in b:
+        if b[c] % 2 == 1:
+            return c
+
