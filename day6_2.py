2ab8d393a7e37cced522fa9f8937481e2782d2ad
MinhWuyn <nguyenduyminh0205@gmail.com>
2024-04-24 14:59:03 +0700

    nothing

---
+++ day6_2.py
@@ -0,0 +1,8 @@
+def unique_in_order(sequence):
+    result = []
+    for i in range(len(sequence)):
+        if i == 0 or sequence[i] != sequence[i - 1]:
+            result.append(sequence[i])
+    return result
+
+print(unique_in_order([1, 2, 3, 3, -1]))
